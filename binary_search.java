cf14df369b82aac1ae5f939e72495efdd70e5e09
Kah Yong <cky734@gmail.com>
2025-06-08 18:39:18 +0800

    update readFiles function

--- binary_search.java
+++ binary_search.java
@@ -1,26 +1,31 @@
-import java.util.Scanner;
-import java.io.File;
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
 public class binary_search {
     public static void main(String[] args) {
-        String fileName = getUserInput();
-        boolean fileIsFound = searchFileName(fileName);
+        String fileName = GetUserInput();
+        boolean fileIsFound = SearchFileName(fileName);
 
         if (fileIsFound) {
             System.out.println(fileName + " was FOUND in the dataset folder.");
         } else {
             System.out.println(fileName + " was NOT FOUND in the dataset folder.");
         }
+
+        System.out.println("test: " + fileName);
+        ParseFile(fileName);
+
     }
 
-    private static String getUserInput() {
+    private static String GetUserInput() {
         Scanner scannerObject = new Scanner(System.in);
         System.out.println("Enter the file name to perform binary search:");
         String fileNameInput = scannerObject.nextLine();
         return fileNameInput;
     }
 
-    private static boolean searchFileName(String targetFileName) {
+    private static boolean SearchFileName(String targetFileName) {
         File datasetFolder = new File("dataSet");
 
         if (!datasetFolder.exists() || !datasetFolder.isDirectory()) {
@@ -42,4 +47,19 @@ public class binary_search {
         return false;
     }
 
+    private static void ParseFile(String fileName) {
+        try {
+            File myObj = new File("dataSet/" + fileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+
+        }
+    }
 }
